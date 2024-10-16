1e8169acd89330925fbc53706557ef85b4334351
yifaaan <0219lyf@gmail.com>
2024-10-16 23:36:52 +0800

    add google test

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include "server.h"
-#include "common.h"
 #include <string>
 #include <iostream>
 
-int main()
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(dbserver, BasicAssertions)
 {
     auto db = cl::Db("hello.db");
     db.Open();
     const char* key   = "Liuyifan";
     const char* value = "2024-hello-world";
     bool        ret   = db.Put(key, value);
-    std::cout << ret << std::endl;
-    // REQUIRE(ret == true);
+    // std::cout << ret << std::endl;
+    // Expect two strings not to be equal.
+    // EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(ret, true);
 }
+
+// int main()
+// {
+//     auto db = cl::Db("hello.db");
+//     db.Open();
+//     const char* key   = "Liuyifan";
+//     const char* value = "2024-hello-world";
+//     bool        ret   = db.Put(key, value);
+//     std::cout << ret << std::endl;
+//     // REQUIRE(ret == true);
+// }
